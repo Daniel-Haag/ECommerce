6ec1f5fc5a36dda33b7a03cee410eb103cd0e184
Daniel Haag <danni.haag91@gmail.com>
2022-07-01 00:40:57 -0300

    Iniciando implementação login

---
+++ ECommerce/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ECommerce.Models;
+
+namespace ECommerce.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Usuario usuario)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
