840770a08aa13455c84e5efc0b9194391a3efe32
Daniel Haag <danni.haag91@gmail.com>
2022-08-16 00:48:48 -0300

    Iniciando implementação do cadastro de produtos

---
+++ ECommerce/Controllers/ProdutoController.cs
@@ -0,0 +1,25 @@
+﻿using ECommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NovoProduto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NovoProduto(Produto produto)
+        {
+            return View();
+        }
+    }
+}
